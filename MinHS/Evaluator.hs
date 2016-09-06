6698f85efea6cf9fb231e39b4bea051aa61ba21b
Costa Paraskevopoulos <costa.paras1@gmail.com>
2016-09-06 21:54:13 +1000

    found bug - infinite lists inference rule not handled

--- MinHS/Evaluator.hs
+++ MinHS/Evaluator.hs
@@ -101,12 +101,12 @@ evalE g (App (App (Con "Cons") x) xs) = case (evalE g x) of
   _     -> error "runtime error: type checking must have failed"
 
 -- evaluates empty list inspector
-evalE g (App (Prim Null) (e)) = case (evalE g e) of
+evalE g (App (Prim Null) e) = case (evalE g e) of
   Nil -> B True
   _   -> B False
 
 -- evaluates head
-evalE g (App (Prim Head) (e)) = case (evalE g e) of
+evalE g (App (Prim Head) e) = case (evalE g e) of
   (Cons x xs) -> I x
   Nil         -> error "runtime error: list is empty"
 --  _ -> evalE g (App (Prim Head) (convert (evalE g e))) -- TODO: check this
@@ -114,10 +114,10 @@ evalE g (App (Prim Head) (e)) = case (evalE g e) of
 -- evaluates tail
 evalE g (App (Prim Tail) (App (App (Con "Cons") _) xs)) = evalE g xs
 evalE g (App (Prim Tail) (Con "Nil")) = error "runtime error: list is empty"
---TODO: check this
 evalE g (App (Prim Tail) x) = evalE g (App (Prim Tail) (convert (evalE g x)))
 
 -- evaluates let bindings
+-- type information is ignored
 evalE g (Let [Bind x (_) [] e1] e2) =
   let
     e1' = evalE g e1         -- evaluates the binding expression
@@ -128,13 +128,17 @@ evalE g (Let [Bind x (_) [] e1] e2) =
 evalE g (App e1 e2) =
   let
     v1@(Closure g' f x body) = evalE g e1
-    v2   = evalE g e2
-    g''  = (E.add g' (f, v1))
-    g''' = (E.add g'' (x, v2))
-  in evalE g''' body
+    v2   = evalE g e2          -- evaluates function argument
+    g''  = (E.add g' (f, v1))  -- binds function name in body
+    g''' = (E.add g'' (x, v2)) -- binds function parameter in body
+  in evalE g''' body           -- evaluates function body
 
 -- evaluates function values
-evalE g (Letfun (Bind f (_) [] body)) = evalE g body -- TODO: check this
+-- type information is ignored
+evalE g (Letfun (Bind f (_) [] body)) =
+  let
+    g' = (E.add g (f, f))--(evalE g f)))
+  in evalE g' body -- TODO: check this
 evalE g (Letfun (Bind f (_) [param] body)) = Closure g f param body
 
 -- terminates in error for all other expressions
@@ -143,9 +147,8 @@ evalE _ e = error (show e)
 -- converts evaluated expressions into abstract syntax expressions
 -- used only to recover list constructors
 convert :: Value -> Exp
-convert Nil = Con "Nil"
+convert Nil         = Con "Nil"
 convert (Cons x xs) = (App (App (Con "Cons") (Num x)) (convert xs))
---convert c = error (show c) -- TODO: check this
 
 -- evaluates comparison operators
 evalCmp :: Integer -> Integer -> Op -> Bool
